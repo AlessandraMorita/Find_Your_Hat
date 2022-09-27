9756b32dbee8a6bb1d19f88edf608eda55cf4c19
Alessandra Morita <contactalessandramorita@gmail.com>
2022-09-27 20:27:18 +0200

    Add v2

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
     this.field = field;
+    this.x = 0;
+    this.y = 0;
+    this.foundHat = false;
   }
 
   print() {
@@ -20,55 +23,72 @@ class Field {
     }
     return console.log(fieldImg.join(""));
   }
+
+  inputValid(input) {
+    if(input !== 'r' && input !== 'l' && input !== 'u' && input !== 'd') {
+        console.log('Invalid instruction! Try again :)');
+        return false;
+    } 
+    return true;
+  }
+
+  winOrLost(m, n) {
+    if(m >= 0 && m < this.field.length
+        && n >= 0 && n < this.field[0].length) {
+        if(this.field[m][n] === hat) {
+            console.log("You found your hat!");
+            return true;
+        } else if(this.field[m][n] === hole) {
+            console.log("You fell down a hole! Start again :(");
+            return true;
+        } else {
+            this.field[m][n] = pathCharacter;
+            this.print();
+            this.x = m;
+            this.y = n;
+            return false;
+        }  
+    } else {
+        console.log('Out of the Field! Try again :)');
+        return false;
+    }
+  }
+
+  move(diretion) {
+    let xNew = this.x;
+    let yNew = this.y;
+    switch(diretion) {
+        case 'r':
+            xNew += 0;
+            yNew += 1;
+            break;
+        case 'l':
+            xNew += 0;
+            yNew += -1;
+            break;
+        case 'u':
+            xNew += -1;
+            yNew += 0;
+            break;
+        case 'd':
+            xNew += 1;
+            yNew += 0;
+            break;
+    }
+    return {xNew, yNew};
+  }
+
+  playGame() {
+    this.print();
+    while(!this.foundHat) {
+        let diretion = prompt('r = right | l = left | u = up | d = down | Which way?: ');
+        if(this.inputValid(diretion) === true) {
+            let {xNew, yNew} = this.move(diretion);
+            this.foundHat = this.winOrLost(xNew, yNew);
+        }
+    };
+  }
 }
 
-let x = 0;
-let y = 0;
 const myField = new Field([['*', '░', 'O'], ['░', 'O', '░'], ['░', '^', '░']]);
-myField.print();
-let foundHat = false;
-let xNew = x;
-let yNew = y;
-while(!foundHat) {
-    let diretion = prompt('r = right | l = left | u = up | d = down | Which way?: ');
-    if(diretion !== 'r' && diretion !== 'l' && diretion !== 'u' && diretion !== 'd') {
-        console.log('Invalid instruction! Try again :)');
-    } else {
-        switch(diretion) {
-            case 'r':
-                xNew += 0;
-                yNew += 1;
-                break;
-            case 'l':
-                xNew += 0;
-                yNew += -1;
-                break;
-            case 'u':
-                xNew += -1;
-                yNew += 0;
-                break;
-            case 'd':
-                xNew += 1;
-                yNew += 0;
-                break;
-        }
-        
-        if(xNew >= 0 && xNew < myField.field.length
-            && yNew >= 0 && yNew < myField.field[0].length) {
-            if(myField.field[xNew][yNew] === hat) {
-                console.log("You found your hat!");
-                foundHat = true;
-            } else if(myField.field[xNew][yNew] === hole) {
-                console.log("You fell down a hole! Start again :(");
-                foundHat = true;
-            } else {
-                myField.field[xNew][yNew] = pathCharacter;
-                myField.print();
-                x = xNew;
-                y = yNew;
-            }  
-        } else {
-            console.log('Out of the Field! Try again :)');
-        }
-    }
-};
+myField.playGame();
